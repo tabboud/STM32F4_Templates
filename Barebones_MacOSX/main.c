e89b2fa46015715f734cd300aad17349a4bab143
Tony Abboud <tony.abboud54@gmail.com>
2014-10-07 00:54:51 -0400

    Added asm support

--- Barebones_MacOSX/main.c
+++ Barebones_MacOSX/main.c
@@ -5,16 +5,24 @@ Barebones Blinky example MacOSX using arm-none-eabi and ST_Link
 
 
 #include <stdio.h>
-#include <stdint.h>
-
-
-#include "stm32f4xx_conf.h"
 #include "stm32f4xx.h"
 
-volatile uint32_t time_var1, time_var2;
-// Private function prototypes
-void Delay(volatile uint32_t nCount);
-void initialize();
+
+// ASM function prototypes (defined in CortexM4asmOps.asm)
+extern void turnOnLED(uint32_t, int);
+extern void turnOffLED(uint32_t, int);
+
+volatile uint32_t msTicks;                      /* counts 1ms timeTicks       */
+void SysTick_Handler(void) {
+	msTicks++;
+}
+
+//  Delays number of Systicks (happens every 1 ms)
+static void Delay(__IO uint32_t dlyTicks){                                              
+  uint32_t curTicks = msTicks;
+  while ((msTicks - curTicks) < dlyTicks);
+}
+
 void setSysTick(){
 	// ---------- SysTick timer -------- //
 	 /****************************************
@@ -28,49 +36,38 @@ void setSysTick(){
 	}
 }
 
-int main(void) {
-	setSysTick();
-	initialize();
-
-	GPIO_SetBits(GPIOD, GPIO_Pin_12);
-
-	while(1){
-        GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-        Delay(1000);
-	}
-	return 0;
-}
-
-void initialize() {
+void init() {
 	GPIO_InitTypeDef  GPIO_InitStructure;
 
 	// ---------- GPIO  for LEDS -------- //
 	// GPIOD Periph clock enable
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	// Configure PD12, PD13, PD14 and PD15 in output pushpull mode
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	// Configure PD12, PD13, PD14 in output pushpull mode
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
 }
 
-void SysTick_Handler(void)
-{
-	if (time_var1) {
-		time_var1--;
+
+int main(void) {
+	setSysTick();
+	init();
+
+
+// This is how you call the asm functions from C
+	while(1){
+		turnOnLED(0x40020C00, 13);
+		Delay(1000);
+		turnOffLED(0x40020C00, 13);
+		Delay(1000);
 	}
-	time_var2++;
+
+	return 0;
 }
 
-/*
- * Delay a number of systick cycles (1ms)
- */
-void Delay(volatile uint32_t nCount) {
-	time_var1 = nCount;
-	while(time_var1){};
-}
+
 
