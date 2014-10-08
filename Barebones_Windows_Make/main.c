eee064cf573d7a8205bc207207bae78b5bb880c9
Tony Abboud <tony.abboud54@gmail.com>
2014-10-07 23:22:16 -0400

    Added asm support

--- Barebones_Windows_Make/main.c
+++ Barebones_Windows_Make/main.c
@@ -1,51 +1,46 @@
 /*
-Barebones Blinky example using arm-none-eabi
-on Windows (w/ Makefile)
+Barebones Blinky example for Windows using arm-none-eabi in a Makefile
+
+This is sample code to blink the user LEDs using three different methods:
+1. Use the STD_Periph driver functions
+2. Use the STD_Periph driver structures, NOT functions
+3. Use assembly level functions defined in CortexM4asmOps.asm
 */
-
-
 #include <stdio.h>
-#include <stdint.h>
-
 #include "stm32f4xx.h"
 
+// ASM function prototypes (defined in CortexM4asmOps.asm)
+extern void turnOnLED(uint32_t, int);
+extern void turnOffLED(uint32_t, int);
 
 volatile uint32_t msTicks;                      /* counts 1ms timeTicks       */
-/*----------------------------------------------------------------------------
-  SysTick_Handler
- *----------------------------------------------------------------------------*/
 void SysTick_Handler(void) {
 	msTicks++;
 }
 
-/*----------------------------------------------------------------------------
-  delays number of Systicks (happens every 1 ms)
- *----------------------------------------------------------------------------*/
+//  Delays number of Systicks (happens every 1 ms)
 static void Delay(__IO uint32_t dlyTicks){                                              
   uint32_t curTicks = msTicks;
   while ((msTicks - curTicks) < dlyTicks);
 }
 
 void setSysTick(){
-	// ---------- SysTick timer -------- //
-	 /****************************************
-     *SystemFrequency/1000      1ms         *
-     *SystemFrequency/100000    10us        *
-     *SystemFrequency/1000000   1us         *
-     *****************************************/
+	// ---------- SysTick timer (1ms) -------- //
 	if (SysTick_Config(SystemCoreClock / 1000)) {
-		while (1){};	// Capture error	
+		// Capture error
+		while (1){};
 	}
 }
 
-void init_GPIO(){
+void init_GPIO() {
 	GPIO_InitTypeDef  GPIO_InitStructure;
 
+	// ---------- GPIO  for LEDS -------- //
 	// GPIOD Periph clock enable
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	// Configure PD12 and PD15 in output pushpull mode
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_15;
+	// Configure PD12, PD13, PD14 in output pushpull mode
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
@@ -53,14 +48,37 @@ void init_GPIO(){
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
+
 int main(void) {
 	setSysTick();
 	init_GPIO();
 
-	while(1){
-        GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_15);
-        Delay(1000);		// Wait 1 sec.
+int i=0;
+
+//Blink LED14 using Periph Driver functions
+	for(i=0; i<10; i++){
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+		Delay(1000);	// Wait 1 second
 	}
+
+//Blink LED13 using Periph Driver structures, NOT functions
+	for(i=0; i<10; i++){	
+		GPIOD->BSRRL = GPIO_Pin_13;
+		Delay(1000);
+		GPIOD->BSRRH = GPIO_Pin_13;
+		Delay(1000);
+	}
+
+// Blink LED12 using assembly functions
+	while(1){
+		turnOnLED(0x40020C00, 12);
+		Delay(1000);
+		turnOffLED(0x40020C00, 12);
+		Delay(1000);
+	}
+
 	return 0;
 }
+
+
 
